705826d9ef634f6874415c0bfdb3b2734a088752
myclasses-ab <myclassesab@gmail.com>
2026-05-22 18:30:20 +0530

    refactor: remove repository and domain documentation agents and update relevant entity and configuration files

--- src/main/java/com/classes/Backend/Repository/institute/InstituteRepository.java
+++ src/main/java/com/classes/Backend/Repository/institute/InstituteRepository.java
@@ -28,10 +28,9 @@ public interface InstituteRepository extends JpaRepository<Institute, String> {
         SELECT DISTINCT i.* FROM institutes i
         LEFT JOIN branches b ON b.institute_identifier = i.identifier
         LEFT JOIN institute_courses ic ON ic.institute_identifier = i.identifier
-        LEFT JOIN courses c ON c.identifier = ic.course_identifier
         LEFT JOIN institute_facilities f ON f.institute_identifier = i.identifier
         WHERE i.is_active = true
-          AND (:query IS NULL OR i.name ILIKE CONCAT('%', :query, '%') OR c.name ILIKE CONCAT('%', :query, '%') OR i.tagline ILIKE CONCAT('%', :query, '%') OR i.description ILIKE CONCAT('%', :query, '%'))
+          AND (:query IS NULL OR i.name ILIKE CONCAT('%', :query, '%') OR ic.custom_name ILIKE CONCAT('%', :query, '%') OR i.tagline ILIKE CONCAT('%', :query, '%') OR i.description ILIKE CONCAT('%', :query, '%'))
           AND (:cityIdentifier IS NULL OR b.city_identifier = :cityIdentifier)
           AND (:cityName IS NULL OR b.city_name ILIKE CONCAT('%', :cityName, '%'))
           AND (:minFee IS NULL OR ic.fee_min >= :minFee)
